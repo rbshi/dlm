89e09ec322ff2662d0ac4e1ec53ffc30dfea8341
Runbin Shi <runbin.shi@inf.ethz.ch>
2022-02-25 17:05:09 +0100

    Symmetric system sim flow

--- src/main/scala/hwsys/dlm/NodeWrap.scala
+++ src/main/scala/hwsys/dlm/NodeWrap.scala
@@ -18,6 +18,12 @@ class NodeWrap(implicit sysConf: SysConfig) extends Component {
     val done = out Vec(Bool(), sysConf.nTxnMan)
     val cntTxnCmt, cntTxnAbt, cntTxnLd = out Vec(UInt(32 bits), sysConf.nTxnMan)
     val cntClk = out Vec(UInt(40 bits), sysConf.nTxnMan)
+
+    // network
+    val sendQ = master Stream Bits(512 bits)
+    val respQ = master Stream Bits(512 bits)
+    val reqQ = slave Stream Bits(512 bits)
+    val recvQ = slave Stream Bits(512 bits)
   }
 
   val txnManAry = Array.fill(sysConf.nTxnMan)(new TxnManCS(sysConf))
@@ -50,18 +56,27 @@ class NodeWrap(implicit sysConf: SysConfig) extends Component {
   // two node wrap
   // TODO: lkReqRmt arbiter with nId
   // val arbFlowAry = Array.fill(sysConf.nNode-1)(new ArbDataFlow(sysConf.nTxnMan))
-  val arbFlow = new ArbDataFlow(sysConf.nTxnMan)
+  // val arbFlow = new ArbDataFlow(sysConf.nTxnMan)
   val txnAgent = new TxnAgent(sysConf)
+  val sendArb = new SendArbiter(sysConf.nTxnMan)
+  val recvDisp = new RecvDispatcher(sysConf.nTxnMan)
+  val reqDisp = new ReqDispatcher(sysConf.nTxnMan)
+  val respArb = new RespArbiter(sysConf.nTxnMan)
 
-  (arbFlow.sendArb.io.lkReqV, txnManAry).zipped.foreach(_ <> _.io.lkReqRmt)
-  (arbFlow.sendArb.io.wrDataV, txnManAry).zipped.foreach(_ <> _.io.wrRmt)
-  (arbFlow.recvDisp.io.lkRespV, txnManAry).zipped.foreach(_ <> _.io.lkRespRmt)
-  (arbFlow.recvDisp.io.rdDataV, txnManAry).zipped.foreach(_ <> _.io.rdRmt)
+  sendArb.io.sendQ <> io.sendQ
+  recvDisp.io.recvQ <> io.recvQ
+  reqDisp.io.reqQ <> io.reqQ
+  respArb.io.respQ <> io.respQ
 
-  arbFlow.reqDisp.io.wrData <> txnAgent.io.wrData
-  arbFlow.reqDisp.io.lkReq <> txnAgent.io.lkReq
-  arbFlow.respArb.io.rdData <> txnAgent.io.rdData
-  arbFlow.respArb.io.lkResp <> txnAgent.io.lkResp
+  (sendArb.io.lkReqV, txnManAry).zipped.foreach(_ <> _.io.lkReqRmt)
+  (sendArb.io.wrDataV, txnManAry).zipped.foreach(_ <> _.io.wrRmt)
+  (recvDisp.io.lkRespV, txnManAry).zipped.foreach(_ <> _.io.lkRespRmt)
+  (recvDisp.io.rdDataV, txnManAry).zipped.foreach(_ <> _.io.rdRmt)
+
+  reqDisp.io.wrData <> txnAgent.io.wrData
+  reqDisp.io.lkReq <> txnAgent.io.lkReq
+  respArb.io.rdData <> txnAgent.io.rdData
+  respArb.io.lkResp <> txnAgent.io.lkResp
 
   txnAgent.io.ltReq <> ltMCh.io.lt(sysConf.nTxnMan).lkReq
   txnAgent.io.ltResp <> ltMCh.io.lt(sysConf.nTxnMan).lkResp
--- src/main/scala/hwsys/dlm/test/NodeWrapSim.scala
+++ src/main/scala/hwsys/dlm/test/NodeWrapSim.scala
@@ -8,8 +8,42 @@ import hwsys.dlm._
 import hwsys.sim._
 import hwsys.util.Helpers._
 
+class NodeIO(implicit sysConf: SysConfig) extends Bundle {
+
+  val nodeId = in UInt(sysConf.wNId bits)
+  val axi = Vec(master(Axi4(sysConf.axiConf)), sysConf.nTxnMan + sysConf.nNode -1)
+  val cmdAxi = Vec(master(Axi4(sysConf.axiConf)), sysConf.nTxnMan)
+  val start = in Bool()
+  val txnNumTotal = in UInt(32 bits)
+  val cmdAddrOffs = in Vec(UInt(32 bits), sysConf.nTxnMan) //NOTE: unit size 64B
+
+  val sendQ = master Stream Bits(512 bits)
+  val respQ = master Stream Bits(512 bits)
+  val reqQ = slave Stream Bits(512 bits)
+  val recvQ = slave Stream Bits(512 bits)
+}
+
+
 class TwoNodeTop(implicit sysConf: SysConfig) extends Component {
+
+
+  val io = Array.fill(2)(new NodeIO())
   val n0, n1 = new NodeWrap()
+
+  Seq(n0, n1).zipWithIndex.foreach { case(n, idx) =>
+    n.io.nodeId <> io(idx).nodeId
+    n.io.axi <> io(idx).axi
+    n.io.cmdAxi <> io(idx).cmdAxi
+    n.io.start <> io(idx).start
+    n.io.txnNumTotal <> io(idx).txnNumTotal
+    n.io.cmdAddrOffs <> io(idx).cmdAddrOffs
+
+    n.io.sendQ <> io(idx).sendQ
+    n.io.respQ <> io(idx).respQ
+    n.io.reqQ  <> io(idx).reqQ
+    n.io.recvQ <> io(idx).recvQ
+  }
+
 }
 
 object NodeWrapSim {
@@ -29,10 +63,6 @@ object NodeWrapSim {
 
       Seq(dut.n0, dut.n1).foreach { n =>
         n.io.simPublic()
-        n.arbFlow.sendArb.io.simPublic()
-        n.arbFlow.respArb.io.simPublic()
-        n.arbFlow.reqDisp.io.simPublic()
-        n.arbFlow.recvDisp.io.simPublic()
       }
       dut
     }.doSim("nodewrapsim", 99) { dut =>
@@ -44,55 +74,56 @@ object NodeWrapSim {
       val txnCnt = 256
       val txnMaxLen = sysConf.maxTxnLen-1
 
-      Seq(dut.n0, dut.n1).zipWithIndex.foreach {case (n, idx) =>
+      for (idx <- 0 until 2) {
         for (iTxnMan <- 0 until sysConf.nTxnMan) {
-
           // cmd memory
-          val fNId = (i: Int, j: Int) => idx
+          val fNId = (i: Int, j: Int) => 0
           val fCId = (i: Int, j: Int) => 0
           // for different txnMan, there'll be a tIdOffs in txnEntrySimInt
-          val fTId = (i: Int, j: Int) => i*txnLen + j
-          val fLkAttr = (i: Int, j: Int) => 0
+          val fTId = (i: Int, j: Int) => j
+          val fLkAttr = (i: Int, j: Int) => 1
           val fWLen = (i: Int, j: Int) => 0
-
           val txnCtx = SimInit.txnEntrySimInt(txnCnt, txnLen, txnMaxLen, 0)(fNId, fCId, fTId, fLkAttr, fWLen).toArray
-          SimDriver.instAxiMemSim(n.io.cmdAxi(iTxnMan), dut.clockDomain, Some(txnCtx))
-
+          SimDriver.instAxiMemSim(dut.io(idx).cmdAxi(iTxnMan), dut.clockDomain, Some(txnCtx))
           // data memory
-          n.io.axi.foreach(SimDriver.instAxiMemSim(_, dut.clockDomain, None))
+          SimDriver.instAxiMemSim(dut.io(idx).axi(iTxnMan), dut.clockDomain, None)
+        }
+        for (iTxnAgent <- sysConf.nTxnMan until sysConf.nTxnMan + sysConf.nNode -1 ) {
+          // data memory
+          SimDriver.instAxiMemSim(dut.io(idx).axi(iTxnAgent), dut.clockDomain, None)
         }
       }
 
-      val l2rSendNet = SimDriver.streamDelayPipe(dut.clockDomain, dut.n0.arbFlow.sendArb.io.sendQ, dut.n1.arbFlow.reqDisp.io.reqQ, 1000)
-      val l2rRecvNet = SimDriver.streamDelayPipe(dut.clockDomain, dut.n1.arbFlow.respArb.io.respQ, dut.n0.arbFlow.recvDisp.io.recvQ, 1000)
+      val l2rSendNet = SimDriver.streamDelayPipe(dut.clockDomain, dut.io(0).sendQ, dut.io(1).reqQ, 1000)
+      val l2rRecvNet = SimDriver.streamDelayPipe(dut.clockDomain, dut.io(1).respQ, dut.io(0).recvQ, 1000)
 
-      val r2lSendNet = SimDriver.streamDelayPipe(dut.clockDomain, dut.n1.arbFlow.sendArb.io.sendQ, dut.n0.arbFlow.reqDisp.io.reqQ, 1000)
-      val r2lRecvNet = SimDriver.streamDelayPipe(dut.clockDomain, dut.n0.arbFlow.respArb.io.respQ, dut.n1.arbFlow.recvDisp.io.recvQ, 1000)
+      val r2lSendNet = SimDriver.streamDelayPipe(dut.clockDomain, dut.io(1).sendQ, dut.io(0).reqQ, 1000)
+      val r2lRecvNet = SimDriver.streamDelayPipe(dut.clockDomain, dut.io(0).respQ, dut.io(1).recvQ, 1000)
 
-      Seq(dut.n0, dut.n1).zipWithIndex.foreach {case (n, idx) =>
-        n.io.nodeId #= idx
-        n.io.start #= false
-        n.io.txnNumTotal #= txnCnt
-        n.io.cmdAddrOffs.foreach(_ #= 0)
+      for (idx <- 0 until 2) {
+        dut.io(idx).nodeId #= idx
+        dut.io(idx).start #= false
+        dut.io(idx).txnNumTotal #= txnCnt
+        dut.io(idx).cmdAddrOffs.foreach(_ #= 0)
       }
 
       // wait the fifo (empty_ptr) to reset
       dut.clockDomain.waitSampling(sysConf.nLock/sysConf.nLtPart+1000)
 
       // start
-      dut.n0.io.start #= true
-      dut.n1.io.start #= true
-      dut.clockDomain.waitSampling()
-      dut.n0.io.start #= false
-      dut.n1.io.start #= false
+      for (idx <- 0 until 2) {
+        dut.io(idx).start #= true
+        dut.clockDomain.waitSampling()
+        dut.io(idx).start #= false
+      }
 
       dut.n0.io.done.foreach(a => dut.clockDomain.waitSamplingWhere(a.toBoolean))
       dut.n1.io.done.foreach(a => dut.clockDomain.waitSamplingWhere(a.toBoolean))
 
-      Seq(dut.n0, dut.n1).foreach { n =>
+      Seq(dut.n0, dut.n1).zipWithIndex.foreach { case(n, idx) =>
         Seq(n.io.cntTxnLd, n.io.cntTxnCmt, n.io.cntTxnAbt, n.io.cntClk).foreach { sigV =>
           sigV.foreach { sig =>
-            println(s"${sig.getName()} = ${sig.toBigInt}")
+            println(s"Node[$idx]  ${sig.getName()} = ${sig.toBigInt}")
           }
         }
       }
